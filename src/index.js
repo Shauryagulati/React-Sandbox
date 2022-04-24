47b464c117deeb1f288fd5e341bc2b3f75b25d27
Shaurya Gulati <Sgulati1020@gmail.com>
2022-04-24 16:14:20 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello, World! </h1>
+    <p>This is my first time being here</p>
+  </div>,
+  document.getElementById("root")
+);
